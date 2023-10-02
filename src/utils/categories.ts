94b5554faf562cce5f1d54d67565f19732085ce8
Gui Silva <adrianon10@gmail.com>
2023-10-02 05:09:32 -0300

    feat: add categories

---
+++ src/utils/categories.ts
@@ -0,0 +1,39 @@
+export const categories = [
+  {
+    id: Math.random(),
+    name: 'Acadêmico',
+    icon: '/academico.png',
+    route: 'academic',
+  },
+  {
+    id: Math.random(),
+    name: 'Musicais',
+    icon: '/musical.png',
+    route: 'musical',
+  },
+  {
+    id: Math.random(),
+    name: 'Festivais',
+    icon: '/festival.png',
+    route: 'festival',
+  },
+  {
+    id: Math.random(),
+    name: 'Animais',
+    icon: '/animais.png',
+    route: 'animals',
+  },
+  {
+    id: Math.random(),
+    name: 'Cultural',
+    icon: '/cultural.png',
+    route: 'cultural',
+  },
+  { id: Math.random(), name: 'Feiras', icon: '/feiras.png', route: 'fairs' },
+  {
+    id: Math.random(),
+    name: 'Gastrônomico',
+    icon: '/gastronomicos.png',
+    route: 'gastronomic',
+  },
+]
