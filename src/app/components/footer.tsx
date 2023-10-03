3935bd15aa27a4360f01dfef8adcd9914d945135
Gui Silva <adrianon10@gmail.com>
2023-10-03 04:40:50 -0300

    feat: add footer components

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <footer className="flex h-16 w-full items-center justify-end bg-primary px-8 pr-32">
+      <div className="flex items-center justify-end gap-6">
+        <h1 className="text-2xl font-bold text-white">Tickets©️ 2023</h1>
+        <span className="text-sm font-light text-white">-</span>
+        <p className="text-sm font-light text-white">
+          Todos os direitos reservados
+        </p>
+      </div>
+    </footer>
+  )
+}
