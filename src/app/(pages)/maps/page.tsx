2a36c72d208aaa9dd97670db5b5936f79b15a1fa
Gui Silva <adrianon10@gmail.com>
2023-10-09 04:47:12 -0300

    feat: add maps page

---
+++ src/app/(pages)/maps/page.tsx
@@ -0,0 +1,11 @@
+import { Map } from '@/app/components/map'
+
+const Maps = () => {
+  return (
+    <div className="container mb-16 mr-8 h-screen pl-8 pr-28 pt-8">
+      <Map />
+    </div>
+  )
+}
+
+export default Maps
