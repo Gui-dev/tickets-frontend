d2f705ed81491f3fa5e00751a699b9c06198807c
Gui Silva <adrianon10@gmail.com>
2023-10-11 04:54:02 -0300

    chore: add image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'https://znilbkcnbqxzdtipchqf.supabase.co/storage/v1/object/public/tickets-bucket/banner/',
+    ],
+  },
+}
 
 module.exports = nextConfig
