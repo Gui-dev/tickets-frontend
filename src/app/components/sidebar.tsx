dc9d340fe3e745efd4ad7e8fe1bee8f7ce3b6460
Gui Silva <adrianon10@gmail.com>
2023-10-11 04:56:30 -0300

    feat: add links

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,13 +1,12 @@
 import Link from 'next/link'
 import { AiOutlineHome } from 'react-icons/ai'
 import { LiaMapMarkerSolid } from 'react-icons/lia'
-import { MdOutlineAddBox, MdOutlinePrivacyTip } from 'react-icons/md'
+import { MdOutlineAddBox } from 'react-icons/md'
 import { FiFilter } from 'react-icons/fi'
-import { BsQuestionCircle } from 'react-icons/bs'
 
 export const Sidebar = () => {
   return (
-    <aside className="fixed bottom-0 right-0 top-16 z-10 h-screen w-[90px] overflow-y-auto bg-gray-200 p-2 text-center text-xs text-primary shadow-sm">
+    <aside className="fixed bottom-0 right-0 top-16 z-10 h-[70%] w-[90px] overflow-y-auto bg-gray-200 p-2 text-center text-xs text-primary shadow-sm">
       <div className="h-full overflow-y-auto px-3 py-4">
         <Link
           href="/"
@@ -17,7 +16,7 @@ export const Sidebar = () => {
           <span>Home</span>
         </Link>
         <Link
-          href="/"
+          href="/maps"
           className="mb-9 flex cursor-pointer flex-col items-center justify-center transition-all hover:translate-y-1"
         >
           <LiaMapMarkerSolid size={30} />
@@ -25,32 +24,18 @@ export const Sidebar = () => {
         </Link>
 
         <Link
-          href="/"
+          href="/create-event"
           className="mb-9 flex cursor-pointer flex-col items-center justify-center transition-all hover:translate-y-1"
         >
           <MdOutlineAddBox size={30} />
           <span>Adicionar Evento</span>
         </Link>
         <Link
-          href="/"
+          href="/filter-event"
           className="mb-9 flex cursor-pointer flex-col items-center justify-center transition-all hover:translate-y-1"
         >
           <FiFilter size={30} />
           <span>Filtrar Evento</span>
-        </Link>
-        <Link
-          href="/"
-          className="mb-9 flex cursor-pointer flex-col items-center justify-center transition-all hover:translate-y-1"
-        >
-          <BsQuestionCircle size={30} />
-          <span>SAC</span>
-        </Link>
-        <Link
-          href="/"
-          className="mb-9 flex cursor-pointer flex-col items-center justify-center transition-all hover:translate-y-1"
-        >
-          <MdOutlinePrivacyTip size={30} />
-          <span>Privacidade</span>
         </Link>
       </div>
     </aside>
