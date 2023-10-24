78cd4bbba9f7ad3821f84d20251ab1f1591a6c6a
Gui Silva <adrianon10@gmail.com>
2023-10-24 03:27:07 -0300

    feat: add quantity, price and subtotal

--- src/app/components/card-buy-ticket.tsx
+++ src/app/components/card-buy-ticket.tsx
@@ -1,28 +1,17 @@
 'use client'
-
-import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 import { Input } from './form/input'
 import { Button } from './form/button'
 import { useMemo, useState } from 'react'
+import { PriceWrapper } from './price-wrapper'
 
 export const CardBuyTicket = () => {
-  const price = 100
   const [quantity, setQuantity] = useState(1)
+  const price = 100
   const subtotal = useMemo(() => {
     return price * quantity
   }, [quantity])
 
-  const handleDecrementQuantity = () => {
-    if (quantity <= 0) {
-      setQuantity(0)
-      return
-    }
-    setQuantity((prev) => prev - 1)
-  }
-
-  const handleIncrementQuantity = () => {
-    setQuantity((prev) => prev + 1)
-  }
+  console.log(quantity)
 
   return (
     <div className="flex w-[85%] flex-col rounded bg-white shadow-lg">
@@ -37,36 +26,23 @@ export const CardBuyTicket = () => {
           </div>
 
           <div className="flex flex-row items-center justify-center gap-3">
-            <button className="flex h-8 w-8 items-center justify-center bg-primary transition-colors hover:bg-green-700">
-              <AiOutlineMinus
-                size={24}
-                color="#FFF"
-                onClick={handleDecrementQuantity}
-              />
-            </button>
-            <div>
-              <span className="text-base font-medium text-gray-800">
-                {quantity}
-              </span>
-            </div>
-            <button className="flex h-8 w-8 items-center justify-center bg-primary transition-colors hover:bg-green-700">
-              <AiOutlinePlus
-                size={24}
-                color="#FFF"
-                onClick={handleIncrementQuantity}
-              />
-            </button>
+            <PriceWrapper quantity={quantity} setQuantity={setQuantity} />
           </div>
         </div>
 
         <div className="my-4 grid w-full grid-cols-2 gap-3">
           <Input placeholder="Cupom de desconto" title="Cupom" />
-          <Input placeholder="R$0,00" title="Subtotal" value={subtotal} />
+          <Input
+            placeholder="R$0,00"
+            title="Subtotal"
+            value={subtotal}
+            onChange={() => subtotal}
+          />
         </div>
 
         <div className="flex flex-col gap-4 pb-8">
-          <Input placeholder="Insira seu nome" title="nome" />
-          <Input type="email" placeholder="Insira seu e-mail" title="nome" />
+          <Input type="text" placeholder="Insira seu nome" title="nome" />
+          <Input type="email" placeholder="Insira seu e-mail" title="e-mail" />
           <Button variant="primary">Finalizar Compra</Button>
         </div>
       </div>
