9ae2ad74873dc1495b66c670fa43a0779bf7b954
Gui Silva <adrianon10@gmail.com>
2023-11-07 03:59:10 -0300

    feat: add helper function handleCreateEvent

--- src/app/(pages)/create-event/page.tsx
+++ src/app/(pages)/create-event/page.tsx
@@ -1,16 +1,120 @@
 'use client'
 
-import { Input } from '@/app/components/form/input'
-import { categories } from '@/utils/categories'
+import { ChangeEvent, useState } from 'react'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { FaSpinner } from 'react-icons/fa'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { toast } from 'react-toastify'
+
+import { categories } from '@/utils/categories'
+import { Input } from '@/app/components/form/input'
+import {
+  createEventValidation,
+  CreateEventValidationData,
+} from '@/validations/create-event-validation'
+import { Button } from '@/app/components/form/button'
+import { fetchWrapper } from '@/utils/fetch-wrapper'
+import {
+  AddressLocationProps,
+  AutoComplete,
+} from '@/app/components/form/autocomplete'
+import { ImagesPreview } from '@/app/components/images-preview'
 
 const CreateEvent = () => {
-  const [preview, setPreview] = useState(false)
+  const [flyers, setFlyers] = useState<File[]>([])
+  const [banner, setBanner] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const navigation = useRouter()
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    setError,
+    setValue,
+  } = useForm<CreateEventValidationData>({
+    resolver: zodResolver(createEventValidation),
+  })
+
+  const handleCreateEvent = async (data: CreateEventValidationData) => {
+    try {
+      setIsLoading(true)
+      const formData = new FormData()
+      const dateFormatted = new Date(`${data.date}T${data.hour}`)
+
+      formData.append('title', data.title)
+      formData.append('description', data.description)
+      formData.append('city', data.city)
+      formData.append('location', data.location)
+      formData.append('date', dateFormatted.toISOString())
+      formData.append('price', data.price as any)
+      formData.append('sector', data.sector)
+      formData.append('categories', data.categories.toString())
+      formData.append('coupons', data.cupom as any)
+      formData.append('banner', data.banner as any)
+      flyers.forEach((flyer) => {
+        formData.append('flyers', flyer)
+      })
+      await fetchWrapper('/events', {
+        method: 'POST',
+        body: formData,
+      })
+      setIsLoading(false)
+      toast.success('Evento criado com sucesso')
+      navigation.push('/')
+    } catch (error) {
+      toast.error('Erro ao tentar criar o evento, tente mais tarde!')
+      setIsLoading(false)
+    }
+  }
+
+  console.log('ERRORS: ', errors)
+
+  const handleAddressSelected = (address: AddressLocationProps) => {
+    const { city, geoLocation } = address
+    setValue('city', city)
+    setValue('location', `${geoLocation.lat},${geoLocation.lng}`)
+  }
+
+  const handleBannerPreview = (event: ChangeEvent<HTMLInputElement>) => {
+    const image = event.target.files?.[0] || null
+    if (image) {
+      const imageURL = URL.createObjectURL(image)
+      setBanner(imageURL)
+    }
+  }
+
+  const handleFlyersPreview = async (event: ChangeEvent<HTMLInputElement>) => {
+    const images = event.target.files
+
+    if (images && images?.length > 3) {
+      setError('flyers', {
+        message: 'Selecione no máximo 3 imagens',
+      })
+      return
+    }
+    if (images) {
+      // convert `FileList` to `File[]`
+      const _images = Array.from(images)
+      setFlyers(_images)
+    }
+  }
+
+  const handleCloseBannerPreview = () => {
+    setBanner('')
+  }
+
+  const handleCloseFlyersPreview = () => {
+    setFlyers([])
+  }
 
   return (
     <section className="container px-8">
-      <form className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2">
+      <form
+        className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2"
+        onSubmit={handleSubmit(handleCreateEvent)}
+      >
         <div className="mb-4 border-r-2 border-blue-300">
           <h1 className="text-2xl font-medium text-primary">
             Adicionar eventos
@@ -27,7 +131,16 @@ const CreateEvent = () => {
               >
                 Título
               </label>
-              <Input placeholder="Insira o nome do evento" id="title" />
+              <Input
+                type="text"
+                placeholder="Insira o nome do evento"
+                id="title"
+                {...register('title', {
+                  required: 'O titulo é obrigátorio',
+                })}
+                error={!!errors.title}
+                errorMessage={errors.title?.message}
+              />
             </div>
             <div className="mb-4 flex flex-col gap-1">
               <label
@@ -36,7 +149,12 @@ const CreateEvent = () => {
               >
                 Endereço
               </label>
-              <Input placeholder="Insira o endereço do evento" id="address" />
+
+              <AutoComplete
+                type="text"
+                placeholder="Insira o endereço do evento"
+                onAddressSelected={handleAddressSelected}
+              />
             </div>
             <div className="mb-4 grid grid-cols-2 gap-3">
               <div className="flex flex-col gap-1">
@@ -50,6 +168,9 @@ const CreateEvent = () => {
                   type="date"
                   placeholder="Insira a data do evento"
                   id="date"
+                  {...register('date')}
+                  error={!!errors.date}
+                  errorMessage={errors.date?.message}
                 />
               </div>
               <div className="flex flex-col gap-1">
@@ -63,6 +184,9 @@ const CreateEvent = () => {
                   type="time"
                   placeholder="Insira a hora do evento"
                   id="time"
+                  {...register('hour')}
+                  error={!!errors.hour}
+                  errorMessage={errors.hour?.message}
                 />
               </div>
             </div>
@@ -70,27 +194,37 @@ const CreateEvent = () => {
               <span className="mb-4 block text-base text-primary">
                 Categoria do Evento
               </span>
-              <div className="grid grid-cols-5 gap-2">
-                {categories.map((category) => {
-                  return (
-                    <div
-                      key={String(category.id)}
-                      className="flex flex-row items-center gap-1"
-                    >
-                      <input
-                        type="checkbox"
-                        value={category.route}
-                        id={category.route}
-                      />
-                      <label
-                        htmlFor={category.route}
-                        className="text-normal text-sm text-primary"
+              <div className="flex flex-col gap-2">
+                <div className="grid grid-cols-5 gap-2">
+                  {categories.map((category) => {
+                    return (
+                      <div
+                        key={String(category.id)}
+                        className="flex flex-row items-center gap-1"
                       >
-                        {category.name}
-                      </label>
-                    </div>
-                  )
-                })}
+                        <input
+                          type="checkbox"
+                          value={category.route}
+                          id={category.route}
+                          {...register('categories', {
+                            required: errors.categories?.message,
+                          })}
+                        />
+                        <label
+                          htmlFor={category.route}
+                          className="text-normal text-sm text-primary"
+                        >
+                          {category.name}
+                        </label>
+                      </div>
+                    )
+                  })}
+                </div>
+                {!!errors.categories && (
+                  <span className="block text-sm text-red-500">
+                    {errors.categories?.message}
+                  </span>
+                )}
               </div>
             </div>
 
@@ -110,25 +244,46 @@ const CreateEvent = () => {
                 >
                   Preço
                 </label>
-                <Input placeholder="Insira o valor do evento" id="price" />
+                <Input
+                  type="text"
+                  placeholder="Insira o valor do evento"
+                  id="price"
+                  {...register('price')}
+                  error={!!errors.price}
+                  errorMessage={errors.price?.message}
+                />
               </div>
               <div className="mb-4 flex flex-col gap-1">
                 <label
-                  htmlFor="price"
+                  htmlFor="sector"
                   className="text-normal text-sm text-primary"
                 >
                   Setor
                 </label>
-                <Input placeholder="Setor" id="price" />
+                <Input
+                  type="text"
+                  placeholder="Setor"
+                  id="sector"
+                  {...register('sector')}
+                  error={!!errors.sector}
+                  errorMessage={errors.sector?.message}
+                />
               </div>
               <div className="mb-4 flex flex-col gap-1">
                 <label
-                  htmlFor="cupon"
+                  htmlFor="cupom"
                   className="text-normal text-sm text-primary"
                 >
                   Cupom
                 </label>
-                <Input placeholder="Cupom" id="cupon" />
+                <Input
+                  type="text"
+                  placeholder="Ex: SUPER10, SUPER20"
+                  id="cupom"
+                  {...register('cupom')}
+                  error={!!errors.cupom}
+                  errorMessage={errors.cupom?.message}
+                />
               </div>
             </div>
 
@@ -143,6 +298,9 @@ const CreateEvent = () => {
                 placeholder="Insira aqui a descrição do seu evento"
                 className="h-[150px] w-full rounded-lg border border-gray-300 bg-white p-2 text-sm font-normal text-gray-800 placeholder-black placeholder-opacity-20 outline-none transition-all focus:ring-1 focus:ring-green-500"
                 id="description"
+                {...register('description', {
+                  required: errors.description?.message,
+                })}
               ></textarea>
             </div>
           </div>
@@ -154,7 +312,7 @@ const CreateEvent = () => {
             Adicione as imagens referentes ao seu evento
           </p>
           <div className="mb-4 mt-8">
-            {preview && (
+            {banner && (
               <>
                 <label
                   htmlFor="banner"
@@ -165,19 +323,27 @@ const CreateEvent = () => {
                 <p className="text-gray-700">
                   Insira uma imagem no formato 336x280
                 </p>
-                <div className="mt-2 h-28 w-full rounded-3xl bg-zinc-300 shadow">
+                <div className="relative mt-2 h-28 w-full rounded-3xl bg-zinc-300 shadow">
                   <Image
-                    src="/logo.png"
+                    src={banner}
                     alt="Image Banner"
                     height={112}
                     width={336}
-                    className="h-full w-full bg-cover bg-center"
+                    className="h-full w-full rounded-3xl bg-cover bg-center"
                   />
+                  <Button
+                    type="button"
+                    variant="primary"
+                    onClick={handleCloseBannerPreview}
+                    className="absolute right-4 top-4 flex h-6 w-6 items-center justify-center rounded-full"
+                  >
+                    <span className="text-sm font-semibold text-white">x</span>
+                  </Button>
                 </div>
               </>
             )}
 
-            {!preview && (
+            {!banner && (
               <>
                 <label
                   htmlFor="banner"
@@ -193,8 +359,18 @@ const CreateEvent = () => {
                     type="file"
                     accept="image/*"
                     className="h-28 w-full cursor-pointer rounded-3xl opacity-0"
+                    {...register('banner', {
+                      onChange: (e) => handleBannerPreview(e),
+                    })}
+                    error={!!errors.banner}
+                    errorMessage={errors.banner?.message}
                   />
                 </div>
+                {!!errors.banner && (
+                  <span className="mt-4 text-sm text-red-500">
+                    {errors.banner?.message}
+                  </span>
+                )}
               </>
             )}
 
@@ -207,48 +383,49 @@ const CreateEvent = () => {
               </label>
               <p className="text-gray-700">Insira até 3 flyers</p>
             </div>
-            <div className="grid grid-cols-3 gap-3">
-              <div className="h-28 w-full rounded-3xl bg-zinc-300 shadow">
-                <Input
-                  type="file"
-                  accept="image/*"
-                  className="h-28 w-full cursor-pointer rounded-3xl opacity-0"
-                />
-              </div>
-              <div className="h-28 w-full rounded-3xl bg-zinc-300 shadow">
-                <Input
-                  type="file"
-                  accept="image/*"
-                  className="h-28 w-full cursor-pointer rounded-3xl opacity-0"
-                />
-              </div>
 
+            {flyers.length === 0 && (
               <div className="h-28 w-full rounded-3xl bg-zinc-300 shadow">
                 <Input
+                  className="h-28 w-full cursor-pointer rounded-3xl opacity-0"
                   type="file"
                   accept="image/*"
-                  className="h-28 w-full cursor-pointer rounded-3xl opacity-0"
+                  multiple
+                  onChange={(event) => handleFlyersPreview(event)}
                 />
               </div>
-            </div>
+            )}
 
-            <div className="my-4">
-              <label
-                htmlFor="banner"
-                className="text-base font-medium text-primary"
+            {flyers && flyers.length > 0 && (
+              <div className="relative">
+                <div className="grid w-full grid-cols-3 gap-3">
+                  <ImagesPreview key={String(flyers)} images={flyers} />
+                </div>
+                <Button
+                  type="button"
+                  variant="primary"
+                  onClick={handleCloseFlyersPreview}
+                  className="absolute right-4 top-3 flex h-6 w-6 items-center justify-center rounded-full"
+                >
+                  <span className="text-sm font-semibold text-white">x</span>
+                </Button>
+              </div>
+            )}
+            <div className="mt-8 flex items-center justify-center">
+              <Button
+                type="submit"
+                variant="primary"
+                className="flex w-[70%] items-center justify-center"
               >
-                Mapa do Evento
-              </label>
-              <p className="text-gray-700">
-                Insira o mapa do evento indicando os setores
-              </p>
-              <div className="mt-2 h-48 w-full rounded-3xl bg-zinc-300 shadow">
-                <Input
-                  type="file"
-                  accept="image/*"
-                  className="h-48 w-full cursor-pointer rounded-3xl opacity-0"
-                />
-              </div>
+                {isLoading && (
+                  <FaSpinner size={22} color="#FFF" className="animate-spin" />
+                )}
+                {!isLoading && (
+                  <span className="text-lg font-bold text-white">
+                    Criar Evento
+                  </span>
+                )}
+              </Button>
             </div>
           </div>
         </div>
